4670b31f1ed424937d1c5fa34d3350d9c77494d4
Jordi Anguela <jordi.anguela@gmail.com>
2019-04-24 18:43:28 +0200

    open external links in a new tab

---
+++ js/site.js
@@ -0,0 +1,17 @@
+// CODE FROM: http://fraktar.com/markdown-redcarpet-target-blank/
+(function () {
+    handleLinks()
+})();
+
+function handleLinks () {
+    // Don't open our own site's links in new tabs
+    var host = location.hostname;
+    // Grab and loop over all <a> elements
+    var allLinks = document.querySelectorAll('a');
+    for (var i = 0; i < allLinks.length; ++i) {
+        // if links are external and not empty
+        if (allLinks[i].hostname !== host && allLinks[i].hostname !== '') {
+            allLinks[i].target = '_blank'
+        }
+    }
+}
